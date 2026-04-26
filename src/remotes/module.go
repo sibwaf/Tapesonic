90b290e7e53bc1a10697a953d05048e2a3a196fb
sibwaf <7131069+sibwaf@users.noreply.github.com>
2026-04-27 01:46:06 +0400

    cancel sync tasks for deleted remotes

--- src/remotes/module.go
+++ src/remotes/module.go
@@ -1,7 +1,11 @@
 package remotes
 
 import (
+	"errors"
+	"fmt"
+	"log/slog"
 	"tapesonic/artists"
+	"tapesonic/model"
 	"tapesonic/scheduling"
 	"time"
 
@@ -56,7 +60,11 @@ func (module *RemotesModule) RegisterSchedules(scheduler *scheduling.TaskSchedul
 			TASK_REMOTES_SYNC_SUBSONIC_LIBRARY,
 			retryDelay,
 			func(scheduler *scheduling.TaskScheduler, task scheduling.ScheduledTask, parameters SyncSubsonicLibraryTask) error {
-				if err := module.subsonicSync.SyncLibrary(parameters.UserId, parameters.RemoteId); err != nil {
+				err := module.subsonicSync.SyncLibrary(parameters.UserId, parameters.RemoteId)
+				if errors.Is(err, model.ErrNotFound) {
+					slog.Info(fmt.Sprintf("Got a not-found error while syncing library for remoteId=%s, unregistering remote sync task", parameters.RemoteId))
+					return scheduler.UnregisterTask(task.Id)
+				} else if err != nil {
 					return err
 				}
 
